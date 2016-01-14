f57b200577980acd24f62ba7b9b086204635fa23
Gabriele Della Casa Venturelli <hardsetting@gmail.com>
2016-01-14 11:56:27 +0100

    removed console log

--- assets/js/usModelData.js
+++ assets/js/usModelData.js
@@ -3,14 +3,11 @@ angular.module('yii2-angular-form').directive('usModelData', [function() {
         restrict: 'E',
         require: 'ngModel',
         link: function($scope, $element, $attrs, ngModel) {
-
             ngModel.$parsers.push(function parseView(data) {
-                console.log('parsing', data);
                 return data;
             });
 
             ngModel.$setViewValue(JSON.parse($element.text()));
-
             $element.remove();
         }
     };
